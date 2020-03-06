f55d92d38e13f6151e027c81af05aea9038d4aba
Pierre DAGNIAUX <pierredag6@gmail.com>
2020-03-06 12:03:14 +0100

    Création des routes et configuration .env

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('materials/get','MaterialController@get');
+
+Route::post('materials/update/{id}','MaterialController@update');
+
+Route::post('materials/create','MaterialController@create');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
